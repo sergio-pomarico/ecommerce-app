3f1c7f4ec296a376e36103314435c683c7006ff5
Sergio Pomárico <sergiodavid21@gmail.com>
2022-02-03 17:12:46 -0500

    add assets and content for the onboarding

---
+++ src/screens/constanst.ts
@@ -0,0 +1,17 @@
+export const slides = [
+  {
+    title: 'Find your Gadget',
+    image: require('../assets/vr.png'),
+    color: '#5956E9',
+  },
+  {
+    title: 'Discover original products',
+    image: require('../assets/girl.png'),
+    color: '#7DCCEC',
+  },
+  {
+    title: 'Hottest deals around the web',
+    image: require('../assets/app.png'),
+    color: '#F7B6C1',
+  },
+];
