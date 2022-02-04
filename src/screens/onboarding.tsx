9b1bf714a24fe3a4216581cad0e3b1c0c533a884
Sergio Pomárico <sergiodavid21@gmail.com>
2022-02-04 06:53:00 -0500

    onboarding page

--- src/screens/onboarding.tsx
+++ src/screens/onboarding.tsx
@@ -1,12 +1,117 @@
+import React, {useCallback} from 'react';
+import {
+  Dimensions,
+  NativeScrollEvent,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Animated, {
+  interpolateColor,
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+} from 'react-native-reanimated';
+
 import {AuthRoutes, StackNavigationProps} from '@core/types/navigation';
-import {Box} from 'components';
-import React from 'react';
+import {Box, Button} from '@components';
+import {useTheme} from '@config/theme';
+
+import Slide from './slide';
+import {slides} from './constanst';
+import Dot from './dot';
+
+const {width} = Dimensions.get('window');
 
 const OnboardingScreen = ({}: StackNavigationProps<
   AuthRoutes,
   'Onboarding'
 >) => {
-  return <Box />;
+  const translateX = useSharedValue(0);
+  const insets = useSafeAreaInsets();
+  const theme = useTheme();
+  const scrollRef = useAnimatedRef<ScrollView>();
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event: NativeScrollEvent) => {
+      translateX.value = event.contentOffset.x;
+    },
+  });
+
+  const currentIndex = useDerivedValue(() => {
+    return translateX.value / (width - 2 * theme.spacing.s);
+  });
+
+  const onPressNext = useCallback(() => {
+    if (currentIndex.value === slides.length - 1) {
+      return;
+    }
+    scrollRef.current?.scrollTo({
+      x: (width - 2 * theme.spacing.s) * (currentIndex.value + 1),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const background = useDerivedValue(() => {
+    return interpolateColor(
+      translateX.value,
+      slides.map((_, i) => i * width),
+      slides.map(slide => slide.color),
+      'RGB',
+    );
+  });
+
+  const bgStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: background.value,
+      flex: 1,
+      paddingBottom: insets.bottom,
+      paddingHorizontal: theme.spacing.s,
+      paddingTop: insets.top,
+    };
+  });
+
+  return (
+    <Animated.View style={bgStyle}>
+      <Animated.ScrollView
+        horizontal
+        onScroll={scrollHandler}
+        pagingEnabled
+        ref={scrollRef as any}
+        scrollEventThrottle={16}
+        showsHorizontalScrollIndicator={false}
+        style={styles.container}>
+        {slides.map((slide, index) => (
+          <Slide
+            title={slide.title}
+            image={slide.image}
+            index={index}
+            currentIndex={currentIndex}
+            key={slide.title.replace('', '_').toLowerCase()}
+          />
+        ))}
+      </Animated.ScrollView>
+      <Box justifyContent="space-around" alignItems="center" flex={0.35}>
+        <Box flexDirection="row">
+          {slides.map(({title}, index) => (
+            <Dot
+              key={`dot__${title.replace('', '_').toLowerCase()}`}
+              {...{index, currentIndex: currentIndex}}
+            />
+          ))}
+        </Box>
+        <Button label="Next" variant="secondary" onPress={onPressNext} />
+      </Box>
+    </Animated.View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.65,
+  },
+});
 
 export default OnboardingScreen;
