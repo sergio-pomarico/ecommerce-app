5332b7215669d7a508851beabfdf3c63e9841b27
Sergio Pomárico <sergiodavid21@gmail.com>
2022-02-27 22:29:07 -0500

    add custom icon font

---
+++ src/config/icons/icon.tsx
@@ -0,0 +1,8 @@
+import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
+import icoMoonConfig from './selection.json';
+
+export const Icon = createIconSetFromIcoMoon(
+  icoMoonConfig,
+  'codeo',
+  'codeo.ttf',
+);
