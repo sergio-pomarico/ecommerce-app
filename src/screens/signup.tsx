6050ff7badc44f78d72fb5ff6e7419348bccdbf4
Sergio Pomárico <sergiodavid21@gmail.com>
2022-03-02 09:33:34 -0500

    add sign up screen

--- src/screens/signup.tsx
+++ src/screens/signup.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 import {AuthRoutes, StackNavigationProps} from '@core/types/navigation';
-import {Box} from '@components';
+import {Box, Container, Text} from '@components';
+import {useTheme} from '@config/theme';
+import {StyleSheet} from 'react-native';
+import SignUpForm from '@forms/signup';
 
 const OnboardingScreen = ({}: StackNavigationProps<AuthRoutes, 'SignUp'>) => {
-  return <Box />;
+  const theme = useTheme();
+  return (
+    <Container background={theme.colors.primary}>
+      <Box
+        style={StyleSheet.absoluteFill as object}
+        backgroundColor="primary"
+      />
+      <Box paddingHorizontal="l" paddingVertical="xl">
+        <Text variant="hero" textAlign="left">
+          Create Account
+        </Text>
+      </Box>
+      <SignUpForm onSubmit={() => {}} />
+    </Container>
+  );
 };
 
 export default OnboardingScreen;
