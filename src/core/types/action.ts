15de4bd494a40a73c7ac7e85228b85d5c63170a6
Sergio Pomárico <sergiodavid21@gmail.com>
2022-03-07 22:05:16 -0500

    add type to actions

---
+++ src/core/types/action.ts
@@ -0,0 +1,11 @@
+export interface Action<T, P> {
+  readonly type: T;
+  readonly payload?: P;
+}
+
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P,
+): Action<T, P> {
+  return {type, payload};
+}
