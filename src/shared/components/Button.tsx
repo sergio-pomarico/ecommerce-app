58514be0a6565c929e74e265e81a403eb4dc61ad
Sergio Pomárico <sergiodavid21@gmail.com>
2022-03-27 16:53:44 -0500

    add outline variant

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from 'react';
-import {StyleSheet, TouchableOpacity, ViewStyle} from 'react-native';
-import {useTheme} from '@config/theme';
+import {TouchableOpacity, ViewStyle} from 'react-native';
+import {useTheme, makeStyle, Theme} from '@config/theme';
 import {Text} from '@components';
 
 interface ButtonProps {
   label?: string;
-  variant: 'primary' | 'secondary' | 'blue';
+  variant: 'primary' | 'secondary' | 'outline';
   onPress: () => void;
   style?: ViewStyle;
   size?: 'large' | 'medium';
@@ -19,22 +19,22 @@ const Button: FC<ButtonProps> = ({
   style,
 }) => {
   const theme = useTheme();
+  const styles = useStyles();
   const backgroundColor =
-    variant === 'primary'
-      ? theme.colors.primary
-      : variant === 'secondary'
-      ? theme.colors.white
-      : theme.colors.blue;
+    variant === 'primary' ? theme.colors.primary : theme.colors.white;
   const color =
-    variant === 'primary' || variant === 'blue'
-      ? theme.colors.white
-      : theme.colors.primary;
-  const width = size === 'medium' ? 225 : '100%';
-  const height = size === 'medium' ? 50 : 70;
+    variant === 'primary' ? theme.colors.white : theme.colors.primary;
+  const maxHeight = size === 'medium' ? 50 : 70;
+  const width = size === 'medium' ? undefined : '100%';
 
   return (
     <TouchableOpacity
-      style={[styles.container, style, {backgroundColor, height, width}]}
+      style={[
+        styles.container,
+        style,
+        {backgroundColor, maxHeight, width},
+        variant === 'outline' ? styles.border : {},
+      ]}
       onPress={onPress}>
       <Text
         style={{color}}
@@ -49,14 +49,19 @@ Button.defaultProps = {
   variant: 'primary',
 };
 
-const styles = StyleSheet.create({
+const useStyles = makeStyle((theme: Theme) => ({
   container: {
     alignSelf: 'center',
     alignContent: 'center',
     borderRadius: 10,
+    flex: 1,
     height: 70,
     justifyContent: 'center',
   },
-});
+  border: {
+    borderColor: theme.colors.primary,
+    borderWidth: 1,
+  },
+}));
 
 export default Button;
