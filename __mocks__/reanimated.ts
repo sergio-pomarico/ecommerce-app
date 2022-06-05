267cb5283926a328bf59b85fea56d1ee116107b0
Sergio Pomárico <sergiodavid21@gmail.com>
2022-06-05 18:03:12 -0500

    fix test

--- __mocks__/reanimated.ts
+++ __mocks__/reanimated.ts
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler/jestSetup';
 
-//jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
