d68775ea02723af7f131682327fe5819c6175222
Sergio Pomárico <sergiodavid21@gmail.com>
2022-06-18 16:40:44 -0500

    Pressable atom

--- src/shared/atoms/index.ts
+++ src/shared/atoms/index.ts
@@ -2,6 +2,7 @@ import AnimatedBox from './AnimatedBox';
 import AnimatedScrollView from './AnimatedScrollView';
 import Box from './Box';
 import Icon from './Icon';
+import Pressable from './Pressable';
 import Text from './Text';
 
-export {AnimatedBox, AnimatedScrollView, Box, Icon, Text};
+export {AnimatedBox, AnimatedScrollView, Box, Icon, Pressable, Text};
